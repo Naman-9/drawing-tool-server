ef28f81094fdebc52932f2e30905f3d260061201
Naman-9 <namancp7@gmail.com>
2023-10-16 08:37:50 +0530

    Implemented webSockets

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors({origin: 'http://localhost:3000'}))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, { cors: 'http://localhost:3000' });
 
 io.on("connection", (socket) => {
   // ...connection established
   console.log("server connected");
+
+  socket.on('beginPath', (arg) => {
+    // send event to all other other then sender
+    socket.broadcast.emit('beginPath', arg);
+  });
+
+  socket.on('drawLine', (arg) => {
+    // send event to all other other then sender
+    socket.broadcast.emit('drawLine', arg);
+  });
+
+  socket.on('changeConfig', (arg) => {
+    // send event to all other other then sender
+    socket.broadcast.emit('changeConfig', arg);
+  });
+
+
 });
 
 httpServer.listen(5000);
